484ff3a12251f251919c2156e408b0263b0d1657
edgar <69613366+Svega2019050@users.noreply.github.com>
2021-07-08 08:37:54 -0600

    s

--- Tienda-Online-front/src/app/app-routing.module.ts
+++ Tienda-Online-front/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'navbar', component: NavbarComponent},
+  {path: 'login', component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
